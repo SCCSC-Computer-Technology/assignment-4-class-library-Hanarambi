6edb836cf0a68915c645490bd347497a20cb01db
Hanarambi <98725596+Hanarambi@users.noreply.github.com>
2024-02-19 09:39:16 -0500

    Add files via upload

---
+++ ClassLibraryPdfExporter/ClassLibraryPdfExporter/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection.Metadata;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextDocument = iTextSharp.text.Document;
+namespace PdfExport//  just a little class to export data from program 
+
+{
+    public class PdfExporter
+    {
+        public void ExportDataToPdf(string filePath, string content)
+        {
+            using (FileStream stream = new FileStream(filePath, FileMode.Create))
+            {
+                iTextDocument pdfDoc = new iTextDocument(PageSize.A4, 10f, 10f, 10f, 0f);
+                PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                pdfDoc.Add(new Paragraph(content)); 
+                pdfDoc.Close();
+                stream.Close();
+            }
+        }
+
+    }
+}
